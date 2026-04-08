d006d0a51dc282e6713d91b33ee2fe65556fcc30
Mohamed S. Shaban <a.mshaban@dev.thiqah.sa>
2026-04-08 14:52:24 +0200

    Fix DemoApplicationTests for CI

--- src/test/java/com/mohammed/demo/DemoApplicationTests.java
+++ src/test/java/com/mohammed/demo/DemoApplicationTests.java
@@ -2,12 +2,22 @@ package com.mohammed.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.hibernate.ddl-auto=create-drop",
+    "spring.data.redis.host=localhost",
+    "spring.data.redis.port=6379"
+})
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
